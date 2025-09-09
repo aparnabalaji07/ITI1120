06baa8abfaefca2e8bd15bd9c4b27546d3ae5f1d
aparnabalaji07 <aparna.balaji@gmail.com>
2025-09-09 12:13:07 -0400

    Assignment 0

---
+++ A0_300462188.py
@@ -0,0 +1,6 @@
+# Course: ITI 1120
+# Assignment 0
+# Balaji, Aparna
+# 300462188
+
+print("Printing from a file.")
---
+++ turtle-ex1_300462188.py
@@ -0,0 +1,51 @@
+# Course: ITI 1120
+# Assignment 0
+# Balaji, Aparna
+# 300462188
+
+import turtle
+s=turtle.Screen()
+t=turtle.Turtle()
+
+# Place your code after this line
+
+t.penup()
+t.goto(0,0)
+t.pendown()
+t.circle(10)
+t.penup()
+
+t.goto(0, -30)
+t.pendown()
+t.circle(40)
+t.penup()
+
+t.goto(0, -50)
+t.pendown()
+t.circle(60)
+t.penup()
+
+t.goto(0, -70)
+t.pendown()
+t.circle(80)
+t.penup()
+
+t.goto(0, -150)
+t.pendown()
+t.goto(0, 160)
+t.penup()
+
+t.goto(-150, 10)
+t.pendown()
+t.goto(150,10)
+t.penup()
+
+t.goto(120, 130)
+t.pendown()
+t.goto(-120,-110)
+t.penup()
+
+t.goto(-120, 130)
+t.pendown()
+t.goto(120,-110)
+t.penup()
